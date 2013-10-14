3aa87b0c8bc1d9a0a24d5898b2a70851eb529db3
Nick McDonnough <nickmcd@gmail.com>
2013-10-13 21:28:54 -0400

    Formatting...

--- lib/maple_leaflet/layers.rb
+++ lib/maple_leaflet/layers.rb
@@ -5,25 +5,25 @@
 class Layers < GeoJSON
 
   def marker (coords, caption=nil)
-    count			= @markers.length
-    template	= File.read(File.dirname(__FILE__) + '/templates/marker.js')
-    options		= {
-			:count	=> count,
-			:point	=> coords
+    count     = @markers.length
+    template  = File.read(File.dirname(__FILE__) + '/templates/marker.js')
+    options   = {
+      :count  => count,
+      :point  => coords
     }
     @markers[count] = Mustache.render(template, options)
     caption ? popup("marker#{count}", caption) : return
   end
 
   def circle (coords, radius, caption=nil, style={})
-    count			= @circles.length
-    template	= File.read(File.dirname(__FILE__) + '/templates/circle.js')
-		options		= {
-			:weight			  => 1.5,
-		  :color			  => '#999',
-		  :opacity		  => 1,
-			:fillcolor		=> '#8a5cde',
-    	:fillopacity	=> 0.5
+    count     = @circles.length
+    template  = File.read(File.dirname(__FILE__) + '/templates/circle.js')
+    options   = {
+      :weight       => 1.5,
+      :color        => '#999',
+      :opacity      => 1,
+      :fillcolor    => '#8a5cde',
+      :fillopacity  => 0.5
     }.merge!(style)
     options.merge!({:count => count, :point => coords, :radius => radius})
     @circles[count] = Mustache.render(template, options)
@@ -33,12 +33,12 @@ class Layers < GeoJSON
   def polygon (coords, caption=nil, style={})
     count     = @polygons.length
     template  = File.read(File.dirname(__FILE__) + '/templates/polygon.js')
-	  options   = {
-			:weight			  => 1.5,
-			:color			  => '#999',
-			:opacity		  => 1,
-			:fillcolor		=> '#8a5cde',
-			:fillopacity	=> 0.5
+    options   = {
+      :weight       => 1.5,
+      :color        => '#999',
+      :opacity      => 1,
+      :fillcolor    => '#8a5cde',
+      :fillopacity  => 0.5
     }.merge!(style)
     options.merge!({:count => count, :points => coords})
     @polygons[count] = Mustache.render(template, options)
@@ -46,11 +46,11 @@ class Layers < GeoJSON
   end
 
   def popup (target, caption)
-	  count     = @popups.length
-    template	= File.read(File.dirname(__FILE__) + '/templates/popup.js')
-    options		= {
-			:target   => target,
-			:caption	=> caption,
+    count     = @popups.length
+    template  = File.read(File.dirname(__FILE__) + '/templates/popup.js')
+    options   = {
+      :target   => target,
+      :caption  => caption,
     }
     @popups[count] = Mustache.render(template, options)
   end
